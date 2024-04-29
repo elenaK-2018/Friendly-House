e955e7277751c72f8fff29058ad8b245ad326c3a
ElenaK <elenaKudmax@gmail.com>
2024-04-29 15:29:10 +0300

    added new files

--- js/script.js
+++ js/script.js
@@ -64,3 +64,121 @@ burgerMenu.on('click', function() {
 link.on('click', function() {
     closeMenu()
 });
+
+
+
+// const navigation = $('.menu__wrapper');
+// const navigationClose = $('.header__menu-btn');
+// const presentBtn = $('.btn');
+// const modalOrder = $('.form-block')
+// const modalOrderClose = $('.form-block__close');
+
+// //!--inert 
+
+// let prevActiveElement;
+
+
+// function addInnert(elem) {
+//     prevActiveElement = document.activeElement;
+//     for (let i = 0; i < document.body.children.length; i++) {
+//         if (document.body.children[i] !== elem) {
+//             document.body.children[i].inert = true;
+//         }
+//     };
+//     elem.inert = false;
+//     if (elem.closest('[inert]')) elem.closest('[inert]').inert = false;
+
+//     for (let i = 0; i < window.elemsInert.length; i++) {
+//         if (elem === window.elemsInert[i].elem && window.elemsInert[i].esc) {
+//             function esc(e) {
+//                 if (e.key == 'Escape') {
+//                     window.elemsInert[i].esc()
+//                 }
+//                 document.removeEventListener('keydown', esc);
+//             }
+//             document.addEventListener('keydown', esc);
+//         }
+
+//     }
+// }
+
+// function activationInnert(elemsInert) {
+//     window.elemsInert = elemsInert;
+
+//     function removeInnert(elem) {
+//         if (elem) {
+//             for (let i = 0; i < document.body.children.length; i++) {
+//                 if (document.body.children[i] !== elem) {
+//                     document.body.children[i].inert = false;
+//                 }
+//             };
+//             elem.innert = true;
+//             prevActiveElement.focus();
+//         }
+
+//         for (let i = 0; i < elemsInert.length; i++) {
+//             if (window.screen.width <= elemsInert[i].breakpoints || !elemsInert[i].breakpoints) {
+//                 elemsInert[i].elem.inert = true
+//                 console.log(elemsInert[i]);
+//             }
+//         }
+//     }
+//     removeInnert();
+
+//     return removeInnert
+// }
+
+// //!--end-innert
+
+// const removeInnert = activationInnert([
+//     {
+//         elem: modalOrder.get(0),
+//         esc: closeModal
+//     },
+//     {
+//         elem: navigation.get(0),
+//         esc: closeBurger,
+//     }
+// ]);
+
+
+// function openBurger() {
+//     navigation.addClass('navigation_open').animate({
+//         left: 0,
+//     }, 500, function () {
+//         addInnert(navigation.get(0));//!--активируем inert на navigation
+//         navigationClose.animate({
+//             opacity: 1,
+//         }, 300);
+//     });
+// }
+
+// function closeBurger() {
+//     navigationClose.animate({
+//         opacity: 0,
+//     }, 300, function () {
+//         removeInnert(navigation.get(0))//!--Деактивируем inert 
+//         navigation.animate({
+//             left: '-400px',
+//         }, 500).removeClass('navigation_open');
+//     });
+// }
+
+// headerBurger.on('click', openBurger)
+
+// navigationClose.on('click', closeBurger);
+
+// function openModal() {
+//     modalOrder.show(300);
+//     addInnert(modalOrder.get(0));//!--активируем inert на modalOrder
+// }
+
+// function closeModal() {
+//     modalOrder.hide(300);
+//     removeInnert(modalOrder.get(0))//!--Деактивируем inert 
+// }
+
+// presentBtn.on('click', openModal)
+
+// modalOrderClose.on('click', closeModal)
+
